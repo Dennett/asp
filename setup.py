f23a6c410620dc4c6ed9d9722de82a571f8a5147
Dennett <michael.ramsey@gmail.com>
2017-01-20 17:01:34 -0500

    added to gitignore

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='funniest',
       version='0.1',
       description='Approximate Stochastic Paths',
-#      url='http://github.com/storborg/funniest',
+      url='https://github.com/Dennett/asp',
       author='Michael Ramsey',
       author_email='michael.ramsey@gmail.com',
       license='GNU GPLv3',
